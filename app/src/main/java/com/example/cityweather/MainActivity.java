04fcd8d4240a412af36bb2074b48121da31a5a24
serryjohns <serembaj@gmail.com>
2020-04-20 15:10:54 +0300

    Create Fragments for the UI

--- app/src/main/java/com/example/cityweather/MainActivity.java
+++ app/src/main/java/com/example/cityweather/MainActivity.java
@@ -11,8 +11,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tvhe = (TextView) findViewById(R.id.text_hello_word);
-        String weatherAPIKey = BuildConfig.OPEN_WEATHER_API_KEY;
-        tvhe.setText(weatherAPIKey);
     }
 }
---
+++ app/src/main/java/com/example/cityweather/utils/InjectorUtils.java
@@ -0,0 +1,4 @@
+package com.example.cityweather.utils;
+
+public class InjectorUtils {
+}
