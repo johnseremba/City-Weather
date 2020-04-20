ed56d1a56db1a52d262fdd854b45a3316c01379e
serryjohns <serembaj@gmail.com>
2020-04-20 15:39:26 +0300

    Add drawable resources

--- app/src/main/java/com/example/cityweather/ui/map/MapFragment.java
+++ app/src/main/java/com/example/cityweather/ui/map/MapFragment.java
@@ -10,9 +10,6 @@ import android.view.ViewGroup;
 
 import com.example.cityweather.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class MapFragment extends Fragment {
 
     public MapFragment() {
