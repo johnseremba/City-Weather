f778061ae249b16114179bbcb2b0bcd2156e84ba
serryjohns <serembaj@gmail.com>
2020-04-20 12:32:00 +0300

    Configure gradle build

--- app/src/main/java/com/example/cityweather/MainActivity.java
+++ app/src/main/java/com/example/cityweather/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.cityweather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tvhe = (TextView) findViewById(R.id.text_hello_word);
+        String weatherAPIKey = BuildConfig.OPEN_WEATHER_API_KEY;
+        tvhe.setText(weatherAPIKey);
     }
 }
